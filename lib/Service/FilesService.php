26d8419ffba8c33011be289f108b01719dbff2e6
Maxence Lange <maxence@artificial-owl.com>
2019-01-31 17:38:58 -0100

    check that user exists before init FS

--- lib/Service/FilesService.php
+++ lib/Service/FilesService.php
@@ -188,6 +188,10 @@ class FilesService {
 			return;
 		}
 
+		if ($this->userManager->get($userId) === null) {
+			return;
+		}
+
 		$this->externalFilesService->initExternalFilesForUser($userId);
 		$this->groupFoldersService->initGroupSharesForUser($userId);
 	}
