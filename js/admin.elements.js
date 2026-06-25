eabc0d327fa364caf5f7a67e6b9c336b26afe992
Niko M <nikolay.moskvin@gmail.com>
2026-06-25 12:53:54 +1000

    Fix admin UI without global jQuery
    Signed-off-by: Niko M <nikolay.moskvin@gmail.com>

--- js/admin.elements.js
+++ js/admin.elements.js
@@ -20,31 +20,30 @@ var files_elements = {
 	files_open_result_directly: null,
 
 	init: function () {
-		files_elements.files_div = $('#files');
-		files_elements.files_local = $('#files_local');
-		files_elements.files_external = $('#files_external');
-		files_elements.files_group_folders = $('#files_group_folders');
-		files_elements.files_size = $('#files_size');
-		files_elements.files_office = $('#files_office');
-		files_elements.files_pdf = $('#files_pdf');
-		files_elements.files_open_result_directly = $('#files_open_result_directly');
+		files_elements.files_div = document.getElementById('files');
+		files_elements.files_local = document.getElementById('files_local');
+		files_elements.files_external = document.getElementById('files_external');
+		files_elements.files_group_folders = document.getElementById('files_group_folders');
+		files_elements.files_size = document.getElementById('files_size');
+		files_elements.files_office = document.getElementById('files_office');
+		files_elements.files_pdf = document.getElementById('files_pdf');
+		files_elements.files_open_result_directly = document.getElementById('files_open_result_directly');
 
-		files_elements.files_local.on('change', files_elements.updateSettings);
-		files_elements.files_external.on('change', files_elements.updateSettings);
-		files_elements.files_group_folders.on('change', files_elements.updateSettings);
-		files_elements.files_size.on('change', files_elements.updateSettings);
-		files_elements.files_office.on('change', files_elements.updateSettings);
-		files_elements.files_pdf.on('change', files_elements.updateSettings);
-		files_elements.files_open_result_directly.on('change', files_elements.updateSettings);
+		files_elements.files_local.addEventListener('change', files_elements.updateSettings);
+		files_elements.files_external.addEventListener('change', files_elements.updateSettings);
+		files_elements.files_group_folders.addEventListener('change', files_elements.updateSettings);
+		files_elements.files_size.addEventListener('change', files_elements.updateSettings);
+		files_elements.files_office.addEventListener('change', files_elements.updateSettings);
+		files_elements.files_pdf.addEventListener('change', files_elements.updateSettings);
+		files_elements.files_open_result_directly.addEventListener('change', files_elements.updateSettings);
 	},
 
 
 	updateSettings: function () {
-		fts_admin_settings.tagSettingsAsNotSaved($(this));
+		fts_admin_settings.tagSettingsAsNotSaved(this);
 		files_settings.saveSettings();
 	}
 
 
 };
-
 
