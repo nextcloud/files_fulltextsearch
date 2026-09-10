3c4dd71744bafc753546b19e45264ae89154b772
Carl Schwan <carl@carlschwan.eu>
2026-09-10 16:27:34 +0200

    fix: Use strict mode in in_array
    fix: Use strict mode in in_array
    
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>
    
    [skip ci]

--- lib/Model/FileShares.php
+++ lib/Model/FileShares.php
@@ -74,7 +74,7 @@ class FileShares implements JsonSerializable {
 	 * @return $this
 	 */
 	public function addUser(string $user): FileShares {
-		if (!in_array($user, $this->users)) {
+		if (!in_array($user, $this->users, true)) {
 			array_push($this->users, $user);
 		}
 
@@ -106,7 +106,7 @@ class FileShares implements JsonSerializable {
 	 * @return $this
 	 */
 	public function addGroup(string $group): FileShares {
-		if (!in_array($group, $this->groups)) {
+		if (!in_array($group, $this->groups, true)) {
 			array_push($this->groups, $group);
 		}
 
@@ -138,7 +138,7 @@ class FileShares implements JsonSerializable {
 	 * @return $this
 	 */
 	public function addCircle(string $circle): FileShares {
-		if (!in_array($circle, $this->circles)) {
+		if (!in_array($circle, $this->circles, true)) {
 			array_push($this->circles, $circle);
 		}
 
@@ -170,7 +170,7 @@ class FileShares implements JsonSerializable {
 	 * @return FileShares
 	 */
 	public function addLink(string $link): FileShares {
-		if (!in_array($link, $this->links)) {
+		if (!in_array($link, $this->links, true)) {
 			array_push($this->links, $link);
 		}
 
